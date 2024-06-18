693af65af90f364e43a6f635b39c54746dd6a51e
Sara Ahmad Malik <sarahahmadmalik55@gmail.com>
2024-06-18 16:12:37 +0500

    update

---
+++ server/server.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dialogflow from 'dialogflow';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { v4 } from 'uuid';
+
+
+dotenv.config();
+
+const { json } = bodyParser;
+const { SessionsClient } = dialogflow;
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(json());
+app.use(cors());
+
+const projectId = process.env.PROJECT_ID;
+const credentialsPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+const sessionId = v4()
+const sessionClient = new SessionsClient({ keyFilename: credentialsPath });
+
+app.post('/api/message', async (req, res) => {
+    const { message } = req.body;
+
+    try {
+        const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                text: {
+                    text: message,
+                    languageCode: 'en-US',
+                },
+            },
+        };
+
+        const responses = await sessionClient.detectIntent(request);
+        const result = responses[0].queryResult;
+
+        res.json({ fulfillmentText: result.fulfillmentText });
+    } catch (error) {
+        console.error('Error sending message to Dialogflow:', error);
+        res.status(500).json({ error: 'Failed to process message' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ src/App.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from 'react';
+import { List, Input, Avatar, Spin } from 'antd';
+import { SendOutlined, UserOutlined } from '@ant-design/icons';
+import axios from 'axios';
+import Bot from './image/bot.png';
+import './App.css';
+
+const App = () => {
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState('');
+  const [typing, setTyping] = useState(false);
+
+  useEffect(() => {
+    addBotMessage("Hi, what can I help you with?");
+  }, []);
+
+  const addBotMessage = async (text) => {
+    const newMessages = [...messages, { text, user: false }];
+    setMessages(newMessages);
+
+   
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Simulate typing delay
+
+    try {
+      const response = await axios.post('/api/message', { message: text });
+      const botResponse = response.data.fulfillmentText;
+      setMessages([...newMessages, { text: botResponse, user: false }]);
+    } catch (error) {
+      console.error('Error sending message to backend:', error);
+    } finally {
+     
+    }
+  };
+
+  const handleSendMessage = async () => {
+    if (input.trim() === '') return;
+
+    const newMessages = [...messages, { text: input, user: true }];
+    setMessages(newMessages);
+    setInput('');
+
+    setTyping(true);
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Simulate typing delay
+
+    try {
+      const response = await axios.post('http://localhost:5000/api/message', { message: input });
+      const botResponse = response.data.fulfillmentText;
+      setMessages([...newMessages, { text: botResponse, user: false }]);
+    } catch (error) {
+      console.error('Error sending message to backend:', error);
+    } finally {
+      setTyping(false);
+    }
+  };
+
+  return (
+    <div className='parent'>
+      <div className="chatbot-container">
+        <div className="chat-header">
+          <div className="avatar-container">
+            <img src={Bot} alt="Bot" className="bot-avatar" />
+            <span className="online-indicator"></span>
+          </div>
+          <div className="chat-header-info">
+            <h2>Customer Assistance Bot</h2>
+            <p>Online</p>
+          </div>
+        </div>
+        <div className="messages-container">
+          <List
+            itemLayout="horizontal"
+            dataSource={messages}
+            renderItem={item => (
+              <List.Item className={item.user ? 'user' : 'bot'}>
+                <List.Item.Meta
+                  avatar={<Avatar icon={item.user ? <UserOutlined /> : <img src={Bot} alt="Bot" />} />}
+                  title={null}
+                  description={item.text}
+                />
+              </List.Item>
+            )}
+          />
+          {typing && (
+            <div className='typer'>
+              <Avatar icon={<img src={Bot} alt="Bot" />} />
+            <div className="typing-indicator">
+            <div className="typing-balls"></div>
+            <div className="typing-balls"></div>
+            <div className="typing-balls"></div>
+          </div>
+              </div>
+          )}
+        </div>
+        <div className="input-container">
+          <Input.Search
+            placeholder="Type your message here"
+            enterButton={<SendOutlined />}
+            size="large"
+            value={input}
+            onChange={e => setInput(e.target.value)}
+            onSearch={handleSendMessage}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
